a53f31c4f050cbe7625bf7ba1cca4a2752a4717e
Stephanie Shea <stephcshea@gmail.com>
2018-02-07 14:46:41 +0000

    Complete Assignment without branches

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("connected");
+
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+var cities = require('./routes/cities');
+app.use('/cities', cities);
+
+
+app.get('/', function(request, response) {
+	response.send('Hello world');
+});
+app.get('/name', function(request, response) {
+	var name = 'Stephanie';
+	response.send(name);
+});
+app.get('/surprise', function(request, response) {
+	response.send('Surprise!');
+});
+app.get('/redirect', function(request, response) {
+	response.redirect(301, '/surprise');
+});
+app.get('/date', function(request, response) {
+	var date = new Date();
+	response.send('It is ' + date);
+});
+app.listen(process.env.PORT, function() {
+	console.log('Listening on port ' + process.env.PORT);
+	console.log('Local IP address is ' + process.env.IP);
+});
+
---
+++ public/client.js
@@ -0,0 +1,43 @@
+/* global $ */
+$(function() {
+	$.get('/cities', appendToList);
+
+	function appendToList(cities) {
+		var list = [];
+		var content, city;
+		for (var i in cities) {
+			city = cities[i];
+			content = '<a href="/cities/' + city + '">' + city + '</a>' + " " + '<a href="#" data-city="' + city + '"> DEL</a>';
+			
+			list.push($('<li>', {
+				html: content
+			}));
+		}
+		$('#cityOps').append(list);
+	}
+	$('#addCity').on('submit', function(event) {
+		event.preventDefault();
+		var form = $(this);
+		var cityData = form.serialize();
+		$.ajax({
+			type: 'POST',
+			url: '/cities',
+			data: cityData
+		}).done(function(cityName) {
+			appendToList([cityName]);
+			form.trigger('reset');
+		});
+	});
+	$('#cityOps').on('click', 'a[data-city]', function(event) {
+		if (!confirm('Are you sure?')) {
+			return false;
+		}
+		var target = $(event.currentTarget);
+		$.ajax({
+			type: 'DELETE',
+			url: '/cities/' + target.data('city')
+		}).done(function() {
+			target.parents('li').remove();
+		});
+	});
+});
